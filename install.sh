6fd72ab53ef3dbe00a08293245b3791d1dbb1cd7
Christian Rösch <croesch@users.noreply.github.com>
2012-11-20 15:52:32 +0100

    Added output

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ function link_file {
     source="${PWD}/$1"
     target="${HOME}/${1/_/.}"
 
+    info "Linking" "$source" "$target"
+
+
     if [ -e "${target}" ]; then
         if [ -L "${target}" ]; then
             unlink $target
@@ -18,10 +21,17 @@ function unlink_file {
     source="${PWD}/$1"
     target="${HOME}/${1/_/.}"
 
+    info "Unlinking" "$source" "$target"
+
     if [ -e "${target}.df.bak" ] && [ -L "${target}" ]; then
         unlink ${target}
         mv $target.df.bak $target
     fi
+}
+
+
+function info {
+    echo "$1: '$3' --> '$2'"
 }
 
 if [ "$1" = "restore" ]; then
